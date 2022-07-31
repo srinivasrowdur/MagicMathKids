39f8f1847f4cefb0442b4346978b716800070428
srinivasrowdur <99046278+srinivasrowdur@users.noreply.github.com>
2022-07-31 12:02:57 +0100

    Initial Commit

---
+++ MagicMathKids/MagicMathKidsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagicMathKidsApp.swift
+//  MagicMathKids
+//
+//  Created by Srinivas Rowdur on 7/31/22.
+//
+
+import SwiftUI
+
+@main
+struct MagicMathKidsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
