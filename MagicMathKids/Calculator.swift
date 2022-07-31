9be394e2cd313b1c265bd9955e039beaac6d6d0f
srinivasrowdur <99046278+srinivasrowdur@users.noreply.github.com>
2022-07-31 12:04:35 +0100

    Removed ContentView and added Calculator View

---
+++ MagicMathKids/Calculator.swift
@@ -0,0 +1,20 @@
+//
+//  Calculator.swift
+//  MagicMathKids
+//
+//  Created by Srinivas Rowdur on 7/31/22.
+//
+
+import SwiftUI
+
+struct Calculator: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Calculator_Previews: PreviewProvider {
+    static var previews: some View {
+        Calculator()
+    }
+}
--- MagicMathKids/MagicMathKidsApp.swift
+++ MagicMathKids/MagicMathKidsApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct MagicMathKidsApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            Calculator()
         }
     }
 }
