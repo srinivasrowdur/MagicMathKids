7df3ad0165e796b4b59c5b70703dd22c04b920dc
srinivasrowdur <99046278+srinivasrowdur@users.noreply.github.com>
2022-08-01 12:32:28 +0100

    New Button style added and code simplified

--- MagicMathKids/Views/Calculator.swift
+++ MagicMathKids/Views/Calculator.swift
@@ -8,16 +8,19 @@
 import SwiftUI
 
 struct Calculator: View {
+    var buttonTypes: [[ButtonType]] {
+            [[.allClear, .negative, .percent, .operation(.division)],
+             [.digit(.seven), .digit(.eight), .digit(.nine), .operation(.multiplication)],
+             [.digit(.four), .digit(.five), .digit(.six), .operation(.subtraction)],
+             [.digit(.one), .digit(.two), .digit(.three), .operation(.addition)],
+             [.digit(.zero), .decimal, .equals]]
+        }
+    
     var body: some View {
         VStack {
             Spacer()
-            Text("0")
-                .padding()
-                .foregroundColor(Color.white)
-                .frame(maxWidth: .infinity, alignment: .trailing)
-                .font(.system(size: 88, weight: .light))
-                .lineLimit(1)
-                .minimumScaleFactor(0.2)
+            displayText
+            buttonPad
         }
         .background(Color.black)
     }
@@ -26,5 +29,35 @@ struct Calculator: View {
 struct Calculator_Previews: PreviewProvider {
     static var previews: some View {
         Calculator()
+    }
+}
+
+extension Calculator {
+    
+    private var displayText: some View {
+        Text("0")
+            .padding()
+            .foregroundColor(.white)
+            .frame(maxWidth: .infinity, alignment: .trailing)
+            .font(.system(size: 88, weight: .light))
+            .lineLimit(1)
+            .minimumScaleFactor(0.2)
+    }
+    
+    private var buttonPad: some View {
+        VStack {
+            ForEach(buttonTypes, id: \.self) { row in
+                HStack {
+                    ForEach(row, id: \.self) { buttonType in
+                        Button(buttonType.description) { }
+                            .buttonStyle(CalculatorButtonStyle(
+                                size: 80,
+                                backgroundColor: buttonType.backgroundColor,
+                                foregroundColor: buttonType.foregroundColor)
+                            )
+                    }
+                }
+            }
+        }
     }
 }
