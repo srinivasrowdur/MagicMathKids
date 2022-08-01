f40df616af30c6c3cdec543384fdc0631bf998b2
srinivasrowdur <99046278+srinivasrowdur@users.noreply.github.com>
2022-08-01 10:42:21 +0100

    Improved Button type model

--- MagicMathKids/Models/ButtonType.swift
+++ MagicMathKids/Models/ButtonType.swift
@@ -7,25 +7,34 @@
 
 import Foundation
 
-enum ButtonType {
-    case zero
-    case one
-    case two
-    case three
-    case four
-    case five
-    case six
-    case seven
-    case eight
-    case nine
-    case addition
-    case subtraction
-    case multiplication
-    case division
+enum ButtonType: Hashable, CustomStringConvertible {
+    case digit(_ digit: Digit)
+    case operation(_ operation: ArithmeticOperation)
     case negative
     case percent
     case decimal
     case equals
     case allClear
     case clear
+    
+    var description: String {
+            switch self {
+            case .digit(let digit):
+                return digit.description
+            case .operation(let operation):
+                return operation.description
+            case .negative:
+                return "±"
+            case .percent:
+                return "%"
+            case .decimal:
+                return "."
+            case .equals:
+                return "="
+            case .allClear:
+                return "AC"
+            case .clear:
+                return "C"
+            }
+        }
 }
